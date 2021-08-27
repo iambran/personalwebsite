ec4556d883d887998e42d6aa98c42c4baeea0a7c
bran <kangaroodesign@Kangaroos-MacBook-Air.local>
2021-08-28 01:09:59 +0800

    add layout #2

--- pages/layouts/1.js
+++ pages/layouts/1.js
@@ -1,21 +1,41 @@
-import Head from 'next/head'
-import Meta, {siteTitle} from '../../components/meta'
+import Image from 'next/image'
+import Meta from '../../components/meta'
 import Link from 'next/link'
 import layoutStyle from '../../components/layouts/1.module.scss'
 import generalStyle from '../../components/layouts/general.module.scss'
 
 
-export default function() {
+export default function Layout(){
 
-    const pageTitle = 'My first layout'
+    const pageTitle = 'Layout #1'
     return (
         <>
             <Meta pageTitle={pageTitle}>
             </Meta>
 
-            <section className={layoutStyle.grid}>
-                Hello from 1.js page.
-            </section>
+            <main className={layoutStyle.grid}>
+                <div className={layoutStyle.rectangle}>
+                    <h1>Lorem ipsum</h1>
+                </div>
+                <div className={layoutStyle.title}>
+                    <h2>Modern Design</h2>
+                    <p>Magazine</p>
+                </div>
+                <div className={layoutStyle.imageContainer}>
+                    <Image
+                        priority
+                        src="https://images.unsplash.com/photo-1629997047282-41f678126175?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+                        layout="fill"
+                        alt="An image from Unsplash"
+                    />
+                </div>
+                <div className={layoutStyle.firstP}>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempor sodales velit ut tincidunt. In suscipit faucibus iaculis. Vivamus sed elit nec tellus aliquam interdum eget sit amet augue. Sed tempor turpis ac sem tincidunt sodales.</p>
+                </div>
+                <div className={layoutStyle.secondP}>
+                    <p>Nulla mollis eleifend risus. In non facilisis nibh. Duis aliquet neque et nisl euismod, non efficitur velit viverra. Praesent eleifend diam magna, ac scelerisque dui tincidunt eget. Fusce quis nulla in arcu convallis vehicula.</p>
+                </div>
+            </main>
 
             <Link href="/layouts">
                 <a className={generalStyle.back}>Go back</a>
---
+++ pages/layouts/2.js
@@ -0,0 +1,63 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Meta from '../../components/meta'
+import generalStyle from '../../components/layouts/general.module.scss'
+import layoutStyle from '../../components/layouts/2.module.scss'
+import { styles } from 'ansi-colors'
+import { layout } from 'platform'
+
+export default function Layout() {
+    const pageTitle = 'Layout #2'
+    return (
+        <>
+            <Meta pageTitle={pageTitle}></Meta>
+
+            <main className={layoutStyle.grid}>
+                <div>
+                    <h1 className={layoutStyle.uppercase}>Drawing Rooms</h1>
+                    <p>Suspendisse tempor sodales velit ut tincidunt.</p>
+                </div>
+                <div className={layoutStyle.relative}>
+                    <Image 
+                    src="https://images.unsplash.com/photo-1488654715439-fbf461f0eb8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80"
+                    layout="fill"    
+                    />
+                </div>
+
+                <div>
+                    <h1 className={layoutStyle.uppercase}>Sole Mate</h1>
+                    <p>Sed tempor turpis ac sem tincidunt sodales.</p>
+                </div>
+                <div className={layoutStyle.relative}>
+                    <Image 
+                    src="https://images.unsplash.com/photo-1629910036733-5632b57da4b0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=668&q=80"
+                    layout="fill"
+                    />
+                </div>
+                
+                <div>
+                    <h1 className={layoutStyle.uppercase}>Changing Of The Avant-Garde</h1>
+                    <p>Fusce efficitur porttitor placerat.</p>
+                </div>
+                <div>
+                    <img src="https://images.unsplash.com/photo-1504600770771-fb03a6961d33?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1102&q=80" />
+                </div>
+                
+
+                <div>
+                    <h1 className={layoutStyle.uppercase}>Multiple Exposure</h1>
+                    <p>Duis aliquet neque et nisl euismod, non efficitur velit viverra.</p>
+                </div>
+                <div>
+                    <img src="https://images.unsplash.com/photo-1596520093084-e527c9c388ca?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2689&q=80" />
+                </div>
+
+
+            </main>
+
+            <Link href="/layouts">
+                <a className={generalStyle.back}>Go back</a>
+            </Link>
+        </>
+    )
+}
