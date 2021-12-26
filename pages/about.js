d121e76fecd68f965d1a891b839917db6176d04d
brandon <brandon@awe.media>
2021-12-26 15:46:58 +0800

    change class to className

--- pages/about.js
+++ pages/about.js
@@ -19,7 +19,7 @@ export default function About() {
           我自学前端的经历：最开始是在youtube上面接触到Wordpress的相关视频，引起了我的兴趣，后来又学习了域名dns，服务器搭建以及Wordpress建站相关知识。在熟悉Wordpress的基础上看了很多HTML/CSS/JavaScript的教程视频，绝大多数来自YouTube上面的Traversy Media频道，Brad的教学风格很接地气，也是目前认可度最高的频道之一，强烈推荐。当我有了一定的HTML/CSS/JavaScript基础之后，随后了解了一些CSS框架/库和JavaScript框架/库，但后面发现自己的基础并不是特别好，又返回去把mdn上面关于HTML/CSS/JavaScript的文档重新看一遍，第二次看和第一次有着截然不同的感觉和收获。
         </p> */}
 
-        <div class="links">
+        <div className="links">
           <a href="https://github.com/iambran" target="_blank">Github</a>
           <a href="https://codepen.io/brandonzhang" target="_blank">CodePen</a>
           <a href="https://space.bilibili.com/386350896/video" target="_blank">bilibili</a>
